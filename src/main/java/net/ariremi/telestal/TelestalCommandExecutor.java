4e52b68cc451f23a01b40cd8e5fefae632b8018c
BurstLime <burstlime@gmail.com>
2023-02-04 19:43:31 +0900

    listの完成

--- src/main/java/net/ariremi/telestal/TelestalCommandExecutor.java
+++ src/main/java/net/ariremi/telestal/TelestalCommandExecutor.java
@@ -207,39 +207,12 @@ public class TelestalCommandExecutor implements CommandExecutor {
                     p = 1;
                 }
 
-                //ページ操作処理（前）
-                TextComponent page_previous = new TextComponent(plugin.getConfig().getString("page_previous").replace("&","§"));
-                if(p == 1){
-                    page_previous.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(plugin.getConfig().getString("page_nothing").replace("&","§")).create()));
-                }else{
-                    Integer prev_num = Integer.valueOf(p-1);
-                    page_previous.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(plugin.getConfig().getString("page_previous_text").replace("&","§")).create()));
-                    page_previous.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/telestal list "+prev_num.toString()));
-                }
-                //ページ操作処理（次）
-                TextComponent page_next = new TextComponent(plugin.getConfig().getString("page_next").replace("&","§"));
-                if(p < pages){
-                    Integer next_num = Integer.valueOf(p+1);
-                    page_next.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(plugin.getConfig().getString("page_next_text").replace("&","§")).create()));
-                    page_next.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/telestal list "+next_num));
-                }else{
-                    page_next.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(plugin.getConfig().getString("page_nothing").replace("&","§")).create()));
-                }
-
                 if(args.length == 2 && 0 < p && p <= pages){
-                    BaseComponent[] component = new ComponentBuilder(plugin.getConfig().getString("page_start_line").
-                            replace("&","§").replace("<now>",p.toString()).replace("<pages>",pages.toString())).
-                            append("\n").append(new TelestalList(plugin).PageContent(p-1)).append(page_previous).
-                            append(plugin.getConfig().getString("page_end_line").replace("&","§")).append(page_next).create();
-                    sender.spigot().sendMessage(component);
+                    sender.spigot().sendMessage(new TelestalList(plugin).PageContent(p-1));
                 } else if (pages == 0) {
                     sender.sendMessage(plugin.getConfig().getString("list_nothing").replace("&","§"));
                 } else {
-                    BaseComponent[] component = new ComponentBuilder(plugin.getConfig().getString("page_start_line").
-                            replace("&","§").replace("<now>",p.toString()).replace("<pages>",pages.toString())).
-                            append("\n").append(new TelestalList(plugin).PageContent(0)).append(page_previous).
-                            append(plugin.getConfig().getString("page_end_line").replace("&","§")).append(page_next).create();
-                    sender.spigot().sendMessage(component);
+                    sender.spigot().sendMessage(new TelestalList(plugin).PageContent(0));
                 }
             }
         }
--- src/main/java/net/ariremi/telestal/TelestalList.java
+++ src/main/java/net/ariremi/telestal/TelestalList.java
@@ -1,5 +1,7 @@
 package net.ariremi.telestal;
 
+import net.md_5.bungee.api.chat.*;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,10 +14,10 @@ public class TelestalList {
     }
 
     //ページ内容
-    public String PageContent(Integer page){
+    public TextComponent PageContent(Integer page){
         String[] portal_list = PortalList();
         Integer amount = portal_list.length;
-        List<String> page_content = new ArrayList<String>();
+        List<TextComponent> page_content = new ArrayList<>();
         for(int p = 0; p < GetPages(); p++){
             String content = "";
             if (p == GetPages() - 1){
@@ -24,12 +26,42 @@ public class TelestalList {
                             replace("&","§").replace("<file>",portal_list[(p*6)+c]+"\n");
                 }
             }else {
-                for(int c = 0; c < 6; c++){
+                for(int c = 0; c < 8; c++){
                     content = content + plugin.getConfig().getString("page_syntax").
                             replace("&","§").replace("<file>",portal_list[(p*6)+c]+"\n");
                 }
             }
-            page_content.add(content);
+
+            //ページ操作処理（前）
+            TextComponent page_previous = new TextComponent(plugin.getConfig().getString("page_previous").replace("&","§"));
+            if(p == 0){
+                page_previous.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(plugin.getConfig().getString("page_nothing").replace("&","§")).create()));
+            }else{
+                Integer prev_num = p-1;
+                page_previous.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(plugin.getConfig().getString("page_previous_text").replace("&","§")).create()));
+                page_previous.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/telestal list "+prev_num));
+            }
+            //ページ操作処理（次）
+            TextComponent page_next = new TextComponent(plugin.getConfig().getString("page_next").replace("&","§"));
+            if(p < GetPages() - 1){
+                Integer next_num = p+2;
+                page_next.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(plugin.getConfig().getString("page_next_text").replace("&","§")).create()));
+                page_next.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/telestal list "+next_num));
+            }else{
+                page_next.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(plugin.getConfig().getString("page_nothing").replace("&","§")).create()));
+            }
+            Integer next = Integer.valueOf(p+1);
+
+
+            TextComponent page_message = new TextComponent();
+            page_message.addExtra(plugin.getConfig().getString("page_start_line").
+                    replace("&","§").replace("<now>",next.toString()).replace("<pages>",GetPages().toString()));
+            page_message.addExtra("\n"+content);
+            page_message.addExtra(page_previous);
+            page_message.addExtra(plugin.getConfig().getString("page_end_line").replace("&","§"));
+            page_message.addExtra(page_next);
+
+            page_content.add(page_message);
         }
         return page_content.get(page);
     }
@@ -42,13 +74,13 @@ public class TelestalList {
 
         if(amount == 0){
             pages = 0;
-        } else if(amount <= 6){
+        } else if(amount <= 8){
             pages = 1;
         }else {
-            if(((double)amount/6) % 1 != 0){
-                pages = ((int)amount/6) + 1;
+            if(((double)amount/8) % 1 != 0){
+                pages = ((int)amount/8) + 1;
             }else {
-                pages = amount / 6;
+                pages = amount / 8;
             }
         }
         return pages;
@@ -60,12 +92,12 @@ public class TelestalList {
         Integer amount = portal_list.length;
         Integer last;
 
-        if(amount <= 6){
+        if(amount <= 8){
             last = amount;
-        }else if(((double)amount/6) % 1 != 0){
-            last = amount - ((GetPages() - 1) * 6);
+        }else if(((double)amount/8) % 1 != 0){
+            last = amount - ((GetPages() - 1) * 8);
         }else {
-            last = 6;
+            last = 8;
         }
         return last;
     }
