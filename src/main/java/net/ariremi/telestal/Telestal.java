16123f4a288fbea746baf45dc4261b876a301612
BurstLime <burstlime@gmail.com>
2023-02-09 14:07:56 +0900

    依存関係の追加 HolographicDisplays ProtocolLib

--- src/main/java/net/ariremi/telestal/Telestal.java
+++ src/main/java/net/ariremi/telestal/Telestal.java
@@ -1,12 +1,12 @@
 package net.ariremi.telestal;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
 import java.nio.file.Files;
 
 public final class Telestal extends JavaPlugin {
-
     @Override
     public void onEnable() {
         // Plugin startup logic
@@ -16,6 +16,19 @@ public final class Telestal extends JavaPlugin {
         File portal_file = new File(getDataFolder(), "portal");
         if(!Files.exists(portal_file.toPath())){
             portal_file.mkdir();
+        }
+
+        if(!Bukkit.getPluginManager().isPluginEnabled("HolographicDisplays")){
+            getLogger().severe("HolographicDisplays is not installed or not enabled.");
+            getLogger().severe("*** This plugin will be disabled. ***");
+            this.setEnabled(false);
+            return;
+        }
+        if(!Bukkit.getPluginManager().isPluginEnabled("ProtocolLib")){
+            getLogger().severe("ProtocolLib is not installed or not enabled.");
+            getLogger().severe("*** This plugin will be disabled. ***");
+            this.setEnabled(false);
+            return;
         }
 
         getLogger().info("起動しました！");
---
+++ src/main/java/net/ariremi/telestal/TelestalPortal.java
@@ -0,0 +1,5 @@
+package net.ariremi.telestal;
+
+public class TelestalPortal {
+
+}
